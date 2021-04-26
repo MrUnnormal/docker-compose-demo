4885373b2e04d34856139ab94aff455c6252cc7f
MrUnnormal <jan.kaufmann@liwest.at>
2021-04-26 16:17:36 +0200

    add docker-compose.yml

--- prepare-to-docker.sh
+++ prepare-to-docker.sh
@@ -2,3 +2,8 @@
 
 ./mvnw clean package
 mv target/docker-compose-demo-1.0.0-SNAPSHOT-runner.jar ./src/main/docker/demo.jar
+
+cd src/main/docker
+docker-compose build
+docker-compose up -d
+docker-compose logs
