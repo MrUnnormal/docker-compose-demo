2974b21b8b07caa6b51d5774ca1c1ee0260e33d5
MrUnnormal <jan.kaufmann@liwest.at>
2021-04-26 14:38:35 +0200

    ja

---
+++ src/main/java/at/htl/entity/Person.java
@@ -0,0 +1,46 @@
+package at.htl.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    String name;
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public Person() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
